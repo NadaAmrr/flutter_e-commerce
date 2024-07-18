706e747468ae743390abd5c080fd31a040fa01b1
Nada <nada.amr1267@gmail.com>
2024-07-18 21:52:31 +0300

    start

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,13 @@
+class AppImages {
+  static const String _imgPath = "assets/images";
+  static const String _icPath = "assets/icons";
+  static const String icHeart = "$_icPath/ic_heart.svg";
+  static const String icHeartSelect = "$_icPath/ic_heart_select.svg";
+  static const String icHome = "$_icPath/ic_home.svg";
+  static const String icHomeSelect = "$_icPath/ic_home_select.svg";
+  static const String icUser = "$_icPath/ic_user.svg";
+  static const String icUserSelect = "$_icPath/ic_user_select.svg";
+  static const String icCategory = "$_icPath/ic_category.svg";
+  static const String icCategorySelect = "$_icPath/ic_category_select.svg";
+  static const String img = "$_imgPath/2.png";
+}
---
+++ lib/core/utils/app_string.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  /// Base
+  static const Color main = Color(0xff004182);
+  static const Color white = Color(0xffffffff);
+  static const Color black = Color(0xff000000);
+  /////// color scheme
+  static const Color primary = main;
+  static const Color onPrimary = white;
+  // static const Color onPrimaryDark = ;
+  // static const Color secondary = white;
+  // static const Color onSecondary = black;
+  static const Color error = Color(0xffef1837);
+  static const Color onError = error;
+  // static const Color surface = main;
+  // static const Color onSurface = main;
+
+  /// Text
+  static const Color txtMainLight = Color(0xff06004F);
+  // static const Color txtMainDark= black;
+  static const Color txtWhite = white;
+  static const Color txtBlack = black;
+  static const Color txtBlack70 = Color(0x000000B3);
+  static const Color txtPrimaryLight = Color(0x4d06004f);
+  // static const Color txtPrimaryDark = Color(0xFFE3E3E3);
+  // // static const Color txtSecondaryLight = Color(0xff42505C);
+  // static const Color txtSecondaryLight = black;
+  // static const Color txtSecondaryDark= Color(0xff91afc9);
+  // static const Color txtTertiaryLight = Color(0xff79828B);
+  // static const Color txtTertiaryDark = Color(0xff848688);
+
+  /// Icon
+
+}
